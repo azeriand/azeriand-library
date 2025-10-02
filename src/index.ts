a512649017c87985b4e47812ff06f5ab262e5f79
azeriand <andrearomeracosta4@gmail.com>
2025-10-02 16:10:43 +0200

    migrate input component

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { Button } from './components/button/button';
 export { Badge } from './components/badge/badge';
 export { Checkbox } from './components/checkbox/checkbox';
 export { SectionName } from './components/section-name/section-name';
+export { Input } from './components/input/input';

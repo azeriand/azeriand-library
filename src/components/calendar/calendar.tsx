faa36c9cf2d071831109e7e8e17655853cb9b679
azeriand <andrearomeracosta4@gmail.com>
2025-10-04 17:24:42 +0200

    feat: migrate calendar view

---
+++ src/components/calendar/calendar.tsx
@@ -0,0 +1,57 @@
+import React, { JSX } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { DateCalendar, PickersDay } from '@mui/x-date-pickers';
+import { Card, CardProps } from '../card/card';
+import './calendar.css';
+import dayjs from "dayjs";
+import { PickersDayProps } from '@mui/x-date-pickers';
+
+type CalendarProps = CardProps & {
+    selectedDates?: Date[];
+}
+
+interface CustomDayProps extends PickersDayProps {
+    highlightedDays?: dayjs.Dayjs[];
+}
+
+function CustomDay(props: CustomDayProps) {
+    const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+    const isSelected = highlightedDays.some((date: dayjs.Dayjs) => day.isSame(date, "day"));
+
+    return (
+        <PickersDay
+            {...other}
+            day={day}
+            selected={isSelected}
+            outsideCurrentMonth={outsideCurrentMonth}
+            sx={{
+                ...(isSelected && {
+                    backgroundColor: "white !important", // Make selected day white
+                    color: "black !important", // Text color to black for contrast
+                }),
+            }}
+        />
+    );
+}
+
+export function Calendar({ selectedDates, ...cardProps }: { selectedDates?: Date[] } & CardProps): JSX.Element {
+    const theme = createTheme({
+        palette: {
+            mode: "dark",
+        },
+    });
+
+    return (
+        <Card appearance={cardProps.appearance ?? 'ghost'} noPadding {...cardProps}>
+            <ThemeProvider theme={theme}>
+                <DateCalendar
+                    slots={{ day: CustomDay }}
+                    slotProps={{
+                        day: { highlightedDays: selectedDates ? selectedDates.map(dayjs) : [] } as any,
+                    }}
+                />
+            </ThemeProvider>
+        </Card>
+    );
+}

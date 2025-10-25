e1c3982a77e6ac20119052452411a1b9e003c28a
azeriand <andrearomeracosta4@gmail.com>
2025-10-25 14:03:35 +0200

    configure storybook to add docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,12 +3,11 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)"],
   addons: [
     "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+    "@chromatic-com/storybook"
   ],
   framework: {
     name: "@storybook/react-vite",

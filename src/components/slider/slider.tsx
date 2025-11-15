736202cbfb2c7c1f817bf1f2311ea1fe6bdcbc36
azeriand <andrearomeracosta4@gmail.com>
2025-11-15 18:22:49 +0100

    feature: add className prop to slider component

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -4,12 +4,13 @@ import { Card } from '../card/card';
 
 type SliderProps = {
     defaultValue: number;
+    className?: string;
     onChange?: (event: Event, value: number | number[]) => void;
 }
 
-export function NewSlider({defaultValue, onChange}: SliderProps) {
+export function NewSlider({defaultValue, className, onChange}: SliderProps) {
     return (
-        <Card noPadding appearance='ghost'>
+        <Card noPadding appearance='ghost' className={className}>
             <Box sx={{ width: 150 }}>
                 <Slider defaultValue={defaultValue} aria-label="Default" valueLabelDisplay="auto" onChange={onChange} />
             </Box>

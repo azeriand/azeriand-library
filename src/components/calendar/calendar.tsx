6f6d97f5fee7a40e188c958365542aa3a50d36d1
azeriand <andrearomeracosta4@gmail.com>
2026-01-17 20:23:31 +0100

    style: delete calendar padding

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -43,7 +43,7 @@ export function Calendar({ selectedDates, ...cardProps }: { selectedDates?: Date
     });
 
     return (
-        <Card appearance={cardProps.appearance ?? 'ghost'} noPadding {...cardProps} className="w-fit px-5 pt-7">
+        <Card appearance={cardProps.appearance ?? 'ghost'} noPadding {...cardProps} className="w-fit">
             <ThemeProvider theme={theme}>
                 <DateCalendar
                     slots={{ day: CustomDay }}

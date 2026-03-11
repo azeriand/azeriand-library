79009a579c387001e992f63dfa4242f57b89abad
azeriand <andrearomeracosta4@gmail.com>
2026-03-11 12:28:23 +0100

    style: style button and dropdown

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -29,10 +29,10 @@ export function Dropdown({buttonText, options = [], onSelected}: DropdownProps) 
             <DropdownButton toggle={toggleDropdown} open={open}>{ optionSelected }</DropdownButton>
             {
                 open &&
-                <Card noPadding noBlur appearance='outlined' className="absolute flex flex-col items-center p-[0.5rem] mt-[0.5rem] w-full text-center rounded-lg overflow-y-scroll dropdown-content">
+                <Card noPadding className="absolute flex flex-col items-center p-[0.5rem] mt-[0.5rem] w-full text-center rounded-lg overflow-y-scroll dropdown-content">
                     {
                         options.map(option => (
-                            <div key={option} className='p-[0.5rem] m-[0.1rem] w-full rounded-lg cursor-pointer' onMouseDown={() => itemClicked(option)}>
+                            <div key={option} className='p-[0.5rem] m-[0.1rem] font-bold w-full rounded-lg cursor-pointer' onMouseDown={() => itemClicked(option)}>
                                 {option}
                             </div>
                         ))
